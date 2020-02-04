c0fd9624d6dce02890eceeeb6fa1a47e95c75a31
Samuel Vicente <samuel.eduardo.vicente@gmail.com>
2020-02-04 00:19:07 -0600

    Added home reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import login from "../pages/LoginPage/reducer";
 import signup from "../pages/SignUpPage/reducer";
+import home from "../pages/HomePage/reducer";
 
 const rootReducer = combineReducers({
   login,
-  signup
+  signup,
+  home
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
